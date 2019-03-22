5038193950246d81411bbb26d352e8f130bb62e9
peterhyvonen <peterhyvonen@users.noreply.github.com>
2019-03-22 14:59:54 -0700

    Add files via upload

---
+++ install-paramiko.py
@@ -0,0 +1,9 @@
+set enviromental system variable VS90COMNTOOLS to msbuild path.
+C:\<Python dir>\Lib\distutils\msvc9compiler.py
+add ld_args.append('/MANIFEST') where
+ temp_manifest = os.path.join(
+                build_temp,
+                os.path.basename(output_filename) + ".manifest")
+        ld_args.append('/MANIFESTFILE:' + temp_manifest)
+pip install paramiko
+pip install pycrypto
